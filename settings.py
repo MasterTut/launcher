2812013cc6c386c4dd69806cd2cfc9ecb7f549b4
MasterTut <mtut@protonmail.com>
2025-04-01 13:04:05 -0500

    changing folder structure, adding settings file

---
+++ settings.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+#Setting file
+import os
+
+# Project Root directory for relative paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+resolutionWidth = 1920 
+resolutionHeight = 1080
+#resolutionWidth, resolutionHeight = info.current_w, info.current_h
+
+#Display Settings
+WINDOW_SIZE = (resolutionWidth, resolutionHeight)
+FPS = 60
+
+#Font Settings 
+FONT_PATH = os.path.join(BASE_DIR, "Assets", "Fonts", "JetBrainsMonoNLNerdFontPropo-Regular.ttf")
+FONT_SIZE = 30
+
+# Colors
+BACKGROUND_COLOR = (255, 255, 255)  # White
+TEXT_COLOR = (0, 0, 0)  # Black
+BUTTON_COLOR = (100, 100, 200)  # Blue-ish
+
+
+# Asset paths
+BACKGROUND_IMAGE = os.path.join(BASE_DIR, "Assets", "Images", "background2.jpg")
+TEST_BUTTON_IMAGE = os.path.join(BASE_DIR, "Assets", "Images", "testing.png")
