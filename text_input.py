d8bd63abb7ce9f35103e37aa9c3e81d021ee29b7
MasterTut <mtut@protonmail.com>
2025-04-09 15:09:20 -0500

    changing some stuff about AddAppMenu

--- text_input.py
+++ text_input.py
@@ -44,7 +44,9 @@ class AddAppMenu(Menu):
         self.input_boxes = [TextInput(500, 100 + i * 50, 600, 40,self.surface, field) 
             for i, field in enumerate(self.input_box_fields)]
         self.buttons.append(Button(1000,300,150,40,self.surface, "Submit"))
-    
+    def displayFields(self):
+        for input in self.input_boxes:
+            input.display()
     def submitButtonFunc(self):
         dataFromTextInput = {'name': '', 'image': '', 'cmd': ''}
         self.menuFromInput = self.input_boxes[0].text
@@ -66,47 +68,48 @@ class AddAppMenu(Menu):
         
 
     def navigateFields(self):
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.TEXTINPUT:
-                for input in self.input_boxes:
-                    if input.isActive:
-                        input.text += event.text
-                        input.update_text()
-            
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == pygame.K_TAB:
-                     # Switch active input box
-                    for i, box in enumerate(self.input_boxes):
-                        if box.isActive:
-                            box.isActive = False
-                            next_box = self.input_boxes[(i + 1) % len(self.input_boxes)]
-                            next_box.isActive = True
-                            print(f"Switched to {next_box.name}, isActive: {next_box.isActive}")
-                            break
-                    # Activate the first box if none were active
-                    if not any(box.isActive for box in self.input_boxes):
-                        self.input_boxes[0].isActive = True 
-                                                                                                 
-                elif event.key == pygame.K_BACKSPACE:
+        if self.hide == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.TEXTINPUT:
                     for input in self.input_boxes:
                         if input.isActive:
-                            input.text =  input.text[:-1]
+                            input.text += event.text
                             input.update_text()
-                                                                                                 
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if self.buttons[0].buttonRect.collidepoint(mouse_pos):
-                    self.submitButtonFunc()
-                for input in self.input_boxes:
-                    input.isActive = False
-                    if input.rect.collidepoint(mouse_pos):
-                        input.isActive = True
-                    else:
+                
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_TAB:
+                         # Switch active input box
+                        for i, box in enumerate(self.input_boxes):
+                            if box.isActive:
+                                box.isActive = False
+                                next_box = self.input_boxes[(i + 1) % len(self.input_boxes)]
+                                next_box.isActive = True
+                                print(f"Switched to {next_box.name}, isActive: {next_box.isActive}")
+                                break
+                        # Activate the first box if none were active
+                        if not any(box.isActive for box in self.input_boxes):
+                            self.input_boxes[0].isActive = True 
+                                                                                                     
+                    elif event.key == pygame.K_BACKSPACE:
+                        for input in self.input_boxes:
+                            if input.isActive:
+                                input.text =  input.text[:-1]
+                                input.update_text()
+                                                                                                     
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if self.buttons[0].buttonRect.collidepoint(mouse_pos):
+                        self.submitButtonFunc()
+                    for input in self.input_boxes:
                         input.isActive = False
-
+                        if input.rect.collidepoint(mouse_pos):
+                            input.isActive = True
+                        else:
+                            input.isActive = False
+        else:
+            return
